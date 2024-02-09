4703af77e042edf51057226142ca3e49667cc85a
s20120224 <s20120224@alumnos.itsur.edu.mx>
2024-02-09 14:10:25 -0600

    Aplicación finalizada: Trabajo en segundo plano con WorkManager

--- basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -2,10 +2,14 @@ package com.example.bluromatic.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
 import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.KEY_BLUR_LEVEL
+import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -14,6 +18,10 @@ import kotlinx.coroutines.withContext
 private const val TAG = "BlurWorker"
 class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params){
     override suspend fun doWork(): Result {
+        // ADD THESE LINES
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+        val blurLevel = inputData.getInt(KEY_BLUR_LEVEL, 1)
+
         TODO("Not yet implemented")
         makeStatusNotification(
             applicationContext.resources.getString(R.string.blurring_image),
@@ -23,20 +31,36 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, 
         return withContext(Dispatchers.IO) {
             // return try{
             return@withContext try {
-                // This is an utility function added to emulate slower work.
-                delay(DELAY_TIME_MILLIS)
-                val picture = BitmapFactory.decodeResource(
-                    applicationContext.resources,
-                    R.drawable.android_cupcake
+                // NEW code
+                require(!resourceUri.isNullOrBlank()) {
+                    val errorMessage =
+                        applicationContext.resources.getString(R.string.invalid_input_uri)
+                    Log.e(TAG, errorMessage)
+                    errorMessage
+                }
+                val resolver = applicationContext.contentResolver
+
+                val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri))
                 )
-                val output = blurBitmap(picture, 1)
+
+                // This is an utility function added to emulate slower work.
+                //delay(DELAY_TIME_MILLIS)
+                //val picture = BitmapFactory.decodeResource(
+                //    applicationContext.resources,
+                //    R.drawable.android_cupcake
+                //)
+                //val output = blurBitmap(picture, 1)
+                val output = blurBitmap(picture, blurLevel)
                 // Write bitmap to a temp file
                 val outputUri = writeBitmapToFile(applicationContext, output)
-                makeStatusNotification(
-                    "Output is $outputUri",
-                    applicationContext
-                )
-                Result.success()
+                val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+                //makeStatusNotification(
+                //    "Output is $outputUri",
+                //    applicationContext
+                //)
+                //Result.success()
+                Result.success(outputData)
             } catch (throwable: Throwable) {
                 Log.e(
                     TAG,
