825cc00b692ccdbd7a0b29e8faa90e9312b13664
MyriamHerreraVazquez <herreravazquezmyriam@gmail.com>
2024-02-09 13:47:37 -0600

    Inicio del proyecto WorkManager

--- basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/BlurWorker.kt
+++ basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/BlurWorker.kt
@@ -4,7 +4,10 @@ package com.example.bluromatic
     import androidx.work.CoroutineWorker
     import androidx.work.WorkerParameters
     import android.content.Context
+    import android.graphics.BitmapFactory
+    import com.example.bluromatic.workers.blurBitmap
     import com.example.bluromatic.workers.makeStatusNotification
+    import com.example.bluromatic.workers.writeBitmapToFile
 
 private const val TAG = "BlurWorker"
     class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
@@ -15,6 +18,17 @@ private const val TAG = "BlurWorker"
             )
 
             return try {
+                val picture = BitmapFactory.decodeResource(
+                    applicationContext.resources,
+                    R.drawable.android_cupcake
+                )
+                val output = blurBitmap(picture, 1)
+                // Write bitmap to a temp file
+                val outputUri = writeBitmapToFile(applicationContext, output)
+                makeStatusNotification(
+                    "Output is $outputUri",
+                    applicationContext
+                )
                 Result.success()
             } catch (throwable: Throwable) {
                 Result.failure()
