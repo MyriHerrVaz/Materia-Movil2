4d642efc4cc0f91e23acf47cf6e8134d6b74f59a
MyriHerrVaz <S20120200@alumnos.itsur.edu.mx>
2024-02-15 23:32:03 -0600

    subir

--- basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/BlurWorker.kt
+++ basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/BlurWorker.kt
@@ -5,34 +5,67 @@ package com.example.bluromatic
     import androidx.work.WorkerParameters
     import android.content.Context
     import android.graphics.BitmapFactory
+    import android.net.Uri
+    import android.util.Log
+    import androidx.work.workDataOf
     import com.example.bluromatic.workers.blurBitmap
     import com.example.bluromatic.workers.makeStatusNotification
     import com.example.bluromatic.workers.writeBitmapToFile
+    import kotlinx.coroutines.Dispatchers
+    import kotlinx.coroutines.delay
+    import kotlinx.coroutines.withContext
 
 private const val TAG = "BlurWorker"
     class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
         override suspend fun doWork(): Result {
+            // ADD THESE LINES
+            val resourceUri = inputData.getString(KEY_IMAGE_URI)
+            val blurLevel = inputData.getInt(KEY_BLUR_LEVEL, 1)
+
             makeStatusNotification(
                 applicationContext.resources.getString(R.string.blurring_image),
                 applicationContext
             )
+            return withContext(Dispatchers.IO) {
+                // This is an utility function added to emulate slower work.
 
-            return try {
-                val picture = BitmapFactory.decodeResource(
-                    applicationContext.resources,
-                    R.drawable.android_cupcake
-                )
-                val output = blurBitmap(picture, 1)
-                // Write bitmap to a temp file
-                val outputUri = writeBitmapToFile(applicationContext, output)
-                makeStatusNotification(
-                    "Output is $outputUri",
-                    applicationContext
-                )
-                Result.success()
-            } catch (throwable: Throwable) {
-                Result.failure()
-            }
+
+                return@withContext try {
+                    require(!resourceUri.isNullOrBlank()) {
+                        val errorMessage =
+                            applicationContext.resources.getString(R.string.invalid_input_uri)
+                        Log.e(TAG, errorMessage)
+                        errorMessage
+                    }
+                    val resolver = applicationContext.contentResolver
+                    delay(DELAY_TIME_MILLIS)
+                    /*val picture = BitmapFactory.decodeResource(
+                       // applicationContext.resources,
+                      //  R.drawable.android_cupcake
+                   )*/
+                    val picture = BitmapFactory.decodeStream(
+                        resolver.openInputStream(Uri.parse(resourceUri))
+                    )
+                   // val output = blurBitmap(picture, 1)
+                    val output = blurBitmap(picture, blurLevel)
+                    // Write bitmap to a temp file
+                    val outputUri = writeBitmapToFile(applicationContext, output)
+                    /*makeStatusNotification(
+                      //  "Output is $outputUri",
+                   //     applicationContext
+                    )*/
+
+                    val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+                    Result.success(outputData)
+                } catch (throwable: Throwable) {
+                    Log.e(
+                        TAG,
+                        applicationContext.resources.getString(R.string.error_applying_blur),
+                        throwable
+                    )
+                    Result.failure()
+                }
+                }
             TODO("Not yet implemented")
         }
     }
