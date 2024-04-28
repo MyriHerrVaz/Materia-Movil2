5632903e10c0242316953cd7f8ddf69f1036fc81
MyriHerrVaz <S20120200@alumnos.itsur.edu.mx>
2024-04-27 18:16:36 -0600

    Agregamos Rutas

--- MiLocalizacionYMapas5B/app/src/main/java/net/ivanvega/milocalizacionymapasb/Service/ApiService.kt
+++ MiLocalizacionYMapas5B/app/src/main/java/net/ivanvega/milocalizacionymapasb/Service/ApiService.kt
@@ -1,6 +1,7 @@
 package net.ivanvega.milocalizacionymapasb.Service
 
 
+import net.ivanvega.milocalizacionymapasb.ui.RouteResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
