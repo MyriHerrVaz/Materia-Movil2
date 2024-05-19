de22e1501dad1f6488a4bc83636dcc79c3c2a616
s20120224 <s20120224@alumnos.itsur.edu.mx>
2024-05-18 19:51:39 -0600

    Implementar la interfaz de usuario IOS

--- SpaceTutorial/iosApp/iosApp/ContentView.swift
+++ SpaceTutorial/iosApp/iosApp/ContentView.swift
@@ -26,8 +26,52 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+/*struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}*/
+
+struct ContentView: View {
+    @ObservedObject private(set) var viewModel: ViewModel
+
+    var body: some View {
+        NavigationView {
+            listView()
+            .navigationBarTitle("SpaceX Launches")
+            .navigationBarItems(trailing:
+                Button("Reload") {
+                    self.viewModel.loadLaunches(forceReload: true)
+            })
+        }
+    }
+
+    private func listView() -> AnyView {
+        switch viewModel.launches {
+        case .loading:
+            return AnyView(Text("Loading...").multilineTextAlignment(.center))
+        case .result(let launches):
+            return AnyView(List(launches) { launch in
+                RocketLaunchRow(rocketLaunch: launch)
+            })
+        case .error(let description):
+            return AnyView(Text(description).multilineTextAlignment(.center))
+        }
+    }
 }
+extension ContentView {
+    enum LoadableLaunches {
+        case loading
+        case result([RocketLaunch])
+        case error(String)
+    }
+
+    @MainActor
+    class ViewModel: ObservableObject {
+        @Published var launches = LoadableLaunches.loading
+    }
+}
+
+extension RocketLaunch: Identifiable { }
+
+
